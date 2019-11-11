e268a9c1fc04145a6fb09887a86f96f03779aee3
valdal14 <valdal14@gmail.com>
2019-11-11 18:27:37 +0100

    Changed the setup.py file with new configuration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='widgetwizard',
-    version='0.1.3',
+    version='0.2.0',
     author="Valerio D'Alessio - @valdal14",
     author_email="valerio.dalessio@oracle.com",
     license='MIT',
