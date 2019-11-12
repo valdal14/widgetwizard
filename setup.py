ce4d1e1c0b94d6b6c9c3525150e2e63b0dc920ca
valdal14 <valdal14@gmail.com>
2019-11-12 15:02:13 +0100

    Added a MANIFEST.in File and changed the setup.py configuration

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='widgetwizard',
-    version='0.8.5',
+    version='0.8.7',
     author="Valerio D'Alessio - @valdal14",
     author_email="valerio.dalessio@oracle.com",
     license='MIT',
@@ -27,5 +27,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     python_requires='>=3.7.1',
-    package_data={'widgetwizard': ['config.json']}
+    package_data={'widgetwizard': ['config.json']},
+    data_file="widgetwizard/config.json"
 )
